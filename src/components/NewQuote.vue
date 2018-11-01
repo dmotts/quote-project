044c06933af1afd7d06769c8ba3d6108628df445
Daley <daley.mottley@hotmail.com>
2018-11-01 03:41:07 +0000

    add delete quote method

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -5,6 +5,9 @@
                 <label>Quote</label>
                 <textarea rows="3" class="form-control" v-model="quote"></textarea>
             </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <button class="btn btn-primary" @click.prevent="createNew">AddQuote</button>
+            </div>
         </form>
     </div>
 </template>
@@ -15,6 +18,12 @@ export default {
         return {
             quote: ''
         };
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteAdded', this.quote);
+            this.quote = '';
+        }
     }
 }
 </script>
