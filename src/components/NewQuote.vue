4e56a07da8350d1b71064641e5c2d0803d9116ba
Daley <daley.mottley@hotmail.com>
2018-11-01 03:27:28 +0000

    create add new component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <label>Quote</label>
+                <textarea rows="3" class="form-control" v-model="quote"></textarea>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            quote: ''
+        };
+    }
+}
+</script>
+
+<style>
+    
+</style>
